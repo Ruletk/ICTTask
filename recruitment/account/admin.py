8eb556e1d08f28f2a81c45cbe5fe06698eb9d6de
Ruletk <damir.timergazin@gmail.com>
2023-10-25 03:24:36 +0600

    Added pre-commit Added custom user model

--- recruitment/account/admin.py
+++ recruitment/account/admin.py
@@ -1,3 +1,10 @@
+from account.models import User
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Custom base user admin"""
